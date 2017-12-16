7320d34f00560355ab6de3cb5f9d99f109cd14a5
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-16 20:46:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,10 @@
 def line(deli)
+  
   if deli.any?
-    deli.each do |customer|
-      puts " "
-    end
+    deli_line = deli.enum_with_index.map { |customer, i| i + 1 + ". " + customer }
   else
     puts "The line is currently empty"
   end
+  
 end
+
