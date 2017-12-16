de3a1f995927c3301265b68be490defeafbc5433
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-16 20:55:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(deli)
 
   if deli.any?
-    deli_line = deli.map.with_index { |customer, i| (i + 1) + ". " + customer }
+    deli_line = deli.map.with_index { |customer, i| (i.to_i + 1) + ". " + customer }
     deli_line.unshift("The line is currently: ")
     deli_line.join
     puts deli.line
