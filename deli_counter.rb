c1aa0c18db190fe2451bf401c39f20babdaf6a71
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-16 20:51:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,12 @@
 def line(deli)
 
   if deli.any?
-    deli_line = deli.enum_with_index.map { |customer, i| i + 1 + ". " + customer }
+    deli_line = deli.enum_with_index.map { |customer, i| (i + 1) + ". " + customer }
+    deli_line.unshift("The line is currently: ")
+    deli_line.join
+    puts deli.line
   else
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   end
 
 end
