5ae15288cb71ec00a576e994c8d5a91acf05b13a
Wali Rahman <wali.rahman89@yahoo.com>
2017-12-16 20:53:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 def line(deli)
 
   if deli.any?
-    deli_line = deli.enum_with_index.map { |customer, i| (i + 1) + ". " + customer }
+    deli_line = deli.map.with_index { |customer, i| (i + 1) + ". " + customer }
     deli_line.unshift("The line is currently: ")
     deli_line.join
     puts deli.line
